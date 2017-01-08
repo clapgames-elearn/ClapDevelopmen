95b3be35f3254e424712260626b231e71ab27f1a
clapgames-elearn <clapgames.elearn@gmail.com>
2017-01-08 17:41:39 +0530

    Initial environment setup.

---
+++ index.js
@@ -0,0 +1,21 @@
+const   path = require('path'),
+        express = require('express'),
+        app = express(),
+        appPaths = {
+            'backEnd' : 'nodeApp/',
+            'ui' : 'UI-App/',
+            'build' : 'build/'
+        },
+        port = 3000;
+
+app.use(express.static(__dirname +'/'+ appPaths.ui)); 
+
+//app.use('/app',express.static(__dirname +'/app/')); 
+
+app.listen(port, (err) => {  
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+});
